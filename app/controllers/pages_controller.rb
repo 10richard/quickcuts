c02ab71d14a14893449df75faa17d5389b08d401
Richard Ten <rten04@icloud.com>
2023-07-10 18:13:08 -0400

    Add redirect to user dashboard after sign in/sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: [:user_dashboard]
 
     def home
     end
 
     def user_dashboard
+        @user = User.find(current_user.id)
     end
 end
