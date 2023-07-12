ae3c03902688e9792952c59a423e83432d67a4a3
Richard Ten <rten04@icloud.com>
2023-07-11 21:30:48 -0400

    Add columns to User (first_name and last_name)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_013409) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_012911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_013409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["first_name"], name: "index_users_on_first_name", unique: true
+    t.index ["last_name"], name: "index_users_on_last_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
