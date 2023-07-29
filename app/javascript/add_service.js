6ed177c711bb22b14dbd2a029a2b4f7a97e02c56
Richard Ten <rten04@icloud.com>
2023-07-29 19:10:31 -0400

    Add conditional statement
    Checks if there are 5 services
    If there is more than 5, don't allow user to create anymore

--- app/javascript/add_service.js
+++ app/javascript/add_service.js
@@ -2,12 +2,14 @@ const btn = document.getElementById('add-service');
 const servicesContainer = document.getElementById('services');
 
 btn.addEventListener('click', () => {
-    console.log('YAY');
+    if (servicesContainer.childNodes.length - 1 === 6) return;
+    
     const serviceInput = document.createElement('input');
     servicesContainer.append(addAttributes(serviceInput));
 });
 
 function addAttributes(input) {
+    input.setAttribute('class', 'mb-4')
     input.setAttribute('multiple', 'multiple')
     input.setAttribute('type', 'text');
     input.setAttribute('name', 'listing[services][]');
