311e2986cb03aacffd6a0e2ad650505680a51f83
Richard Ten <rten04@icloud.com>
2023-07-31 12:44:33 -0400

    Add associations between Service and Listing models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,8 @@
 class Listing < ApplicationRecord
 
     belongs_to :barber, class_name: 'User'
+    has_many :services, dependent: :destroy
+    accepts_nested_attributes_for :services, allow_destroy: true
 
     validate :user_is_barber?
     validates :title, :barber_id, :services, presence: true
