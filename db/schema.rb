95e49914bed2fdf96f168f6a2637508d3e119136
Richard Ten <rten04@icloud.com>
2023-07-31 12:41:28 -0400

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_31_163520) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_31_163942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
     t.integer "barber_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.integer "listing_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
