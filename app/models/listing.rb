aa6f1289a9d67364f0cfe453116727313d609f02
Richard Ten <rten04@icloud.com>
2023-08-01 12:27:06 -0400

    Add method for Service model nested attributes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
 
     belongs_to :barber, class_name: 'User'
     has_many :services, dependent: :destroy
-    accepts_nested_attributes_for :services, allow_destroy: true
+    accepts_nested_attributes_for :services
 
     validate :user_is_barber?
     validates :title, :barber_id, :services, presence: true
@@ -10,5 +10,13 @@ class Listing < ApplicationRecord
     def user_is_barber?
         #check if user is a barber
         #if not then user cannot create a listing
+    end
+
+    def services_attributes=(services_attributes)
+        services_attributes.each do |i, s_attributes|
+            if !s_attributes[:name].blank? && !s_attributes[:price].blank?
+                self.services.build(s_attributes)
+            end
+        end
     end
 end
