c5984075132e02d2d008b945c37fb05ec8597a21
Richard Ten <rten04@icloud.com>
2023-08-01 20:12:28 -0400

    Delete condition in services_attributes method
    Add a proc that checks for presence of name and price
    instead

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,10 @@
 class Listing < ApplicationRecord
 
     belongs_to :barber, class_name: 'User'
+    
     has_many :services, dependent: :destroy
-    accepts_nested_attributes_for :services
+    accepts_nested_attributes_for :services, 
+    reject_if: proc { |attributes| attributes[:name].blank? || attributes[:price].blank? }
 
     validate :user_is_barber?
     validates :title, :barber_id, :services, presence: true
@@ -14,9 +16,7 @@ class Listing < ApplicationRecord
 
     def services_attributes=(services_attributes)
         services_attributes.each do |i, s_attributes|
-            if !s_attributes[:name].blank? && !s_attributes[:price].blank?
-                self.services.build(s_attributes)
-            end
+            self.services.build(s_attributes)
         end
     end
 end
