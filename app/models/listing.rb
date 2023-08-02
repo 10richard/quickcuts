ef5ec78165be08b9dc7fad04fe99b1c402e72217
Richard Ten <rten04@icloud.com>
2023-08-02 18:20:15 -0400

    Add method for reject_if
    If the service was removed when editting, it would not
    reflect that deletion onto the object. Needed a method
    that would delete that object if its attributes were empty.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,8 +3,8 @@ class Listing < ApplicationRecord
     belongs_to :barber, class_name: 'User'
     
     has_many :services, dependent: :destroy
-    accepts_nested_attributes_for :services,
-    reject_if: proc { |attributes| attributes[:name].blank? || attributes[:price].blank? }
+    accepts_nested_attributes_for :services, allow_destroy: true,
+    reject_if: :reject_service
 
     validate :user_is_barber?
     validates :title, :barber_id, :services, presence: true
@@ -12,5 +12,16 @@ class Listing < ApplicationRecord
     def user_is_barber?
         #check if user is a barber
         #if not then user cannot create a listing
+    end
+
+    def reject_service(attributes)
+        if attributes[:name].blank? || attributes[:price].blank?
+            if attributes[:id].present?
+                attributes.merge!({:_destroy => 1})
+                false
+            else
+                true
+            end
+        end
     end
 end
