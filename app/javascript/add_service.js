353984c593fd2ef58fa48956b562d8204d85c5e7
Richard Ten <rten04@icloud.com>
2023-08-02 17:59:02 -0400

    Fix the attributes that are set on created elements
    Need to add labels for each input (maybe clean up
    everything with functions)

--- app/javascript/add_service.js
+++ app/javascript/add_service.js
@@ -1,18 +1,44 @@
 const btn = document.getElementById('add-service');
 const servicesContainer = document.getElementById('services');
+let count = 1;
 
 btn.addEventListener('click', () => {
     if (servicesContainer.childNodes.length - 1 === 6) return;
     
-    const serviceInput = document.createElement('input');
-    servicesContainer.append(addAttributes(serviceInput));
+    const nameInput = document.createElement('input');
+    const priceInput = document.createElement('input');
+    const deleteBtn = document.createElement('button');
+    const serviceContainer = document.createElement('div');
+    serviceContainer.append(addNameAttributes(nameInput));
+    serviceContainer.append(addPriceAttributes(priceInput));
+    serviceContainer.append(addBtnAttributes(deleteBtn));
+    servicesContainer.append(serviceContainer);
+    count += 1;
 });
 
-function addAttributes(input) {
-    input.setAttribute('class', 'mb-4')
-    input.setAttribute('multiple', 'multiple')
+function addNameAttributes(input) {
     input.setAttribute('type', 'text');
-    input.setAttribute('name', 'listing[services][]');
-    input.setAttribute('id', 'listing_services')
+    input.setAttribute('name', `listing[services_attributes][${count}][name]`);
+    input.setAttribute('id', `listing_services_attributes_${count}_name`)
+    input.setAttribute('class', 'mb-4')
     return input
 }
+
+function addPriceAttributes(input) {
+    input.setAttribute('type', 'text');
+    input.setAttribute('name', `listing[services_attributes][${count}][price]`);
+    input.setAttribute('id', `listing_services_attributes_${count}_price`)
+    input.setAttribute('class', 'mb-4')
+    return input
+}
+
+function addBtnAttributes(btn) {
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('id', 'remove-service');
+    btn.setAttribute('class', 'bg-red-600 ml-2 w-6 text-center text-white rounded-full hover:opacity-70');
+    btn.textContent = 'x'
+    return btn
+}
+
+//<label for="listing_services_attributes_0_name">Name</label>
+//<label for="listing_services_attributes_0_price">Price</label>
