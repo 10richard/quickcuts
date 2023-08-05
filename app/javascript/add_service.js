bf0075ee0691aba34dc80435bf6670c867400413
Richard Ten <rten04@icloud.com>
2023-08-05 17:54:33 -0400

    Fix value for "count" and add functions for labels

--- app/javascript/add_service.js
+++ app/javascript/add_service.js
@@ -1,26 +1,42 @@
 const btn = document.getElementById('add-service');
 const servicesContainer = document.getElementById('services');
-let count = 1;
+let count = parseInt(servicesContainer.lastElementChild.id.split('_')[3]) + 1;
 
 btn.addEventListener('click', () => {
-    if (servicesContainer.childNodes.length - 1 === 6) return;
+    if (servicesContainer.querySelectorAll('div').length > 4) return;
     
+    const nameLabel = document.createElement('label')
+    const priceLabel = document.createElement('label')
     const nameInput = document.createElement('input');
     const priceInput = document.createElement('input');
     const deleteBtn = document.createElement('button');
     const serviceContainer = document.createElement('div');
+    serviceContainer.append(addLabelAttributes(nameLabel, 'name'));
     serviceContainer.append(addNameAttributes(nameInput));
+    serviceContainer.append(addLabelAttributes(priceLabel, 'price'));
     serviceContainer.append(addPriceAttributes(priceInput));
     serviceContainer.append(addBtnAttributes(deleteBtn));
     servicesContainer.append(serviceContainer);
     count += 1;
 });
 
+function addLabelAttributes(label, name="price") {
+    label.setAttribute('class', 'mr-[0.27rem]')
+    if (name == 'name') {
+        label.setAttribute('for', `listing_services_attributes_${count}_name`);
+        label.textContent = 'Name';
+        return label;
+    };
+    label.setAttribute('for', `listing_services_attributes_${count}_price`);
+    label.textContent = 'Price';
+    return label;
+}
+
 function addNameAttributes(input) {
     input.setAttribute('type', 'text');
     input.setAttribute('name', `listing[services_attributes][${count}][name]`);
     input.setAttribute('id', `listing_services_attributes_${count}_name`)
-    input.setAttribute('class', 'mb-4')
+    input.setAttribute('class', 'mb-4 mr-[0.275rem]');
     return input
 }
 
@@ -28,7 +44,7 @@ function addPriceAttributes(input) {
     input.setAttribute('type', 'text');
     input.setAttribute('name', `listing[services_attributes][${count}][price]`);
     input.setAttribute('id', `listing_services_attributes_${count}_price`)
-    input.setAttribute('class', 'mb-4')
+    input.setAttribute('class', 'mb-4 mr-[0.275rem]');
     return input
 }
 
@@ -36,9 +52,6 @@ function addBtnAttributes(btn) {
     btn.setAttribute('type', 'button');
     btn.setAttribute('id', 'remove-service');
     btn.setAttribute('class', 'bg-red-600 ml-2 w-6 text-center text-white rounded-full hover:opacity-70');
-    btn.textContent = 'x'
+    btn.textContent = 'x';
     return btn
 }
-
-//<label for="listing_services_attributes_0_name">Name</label>
-//<label for="listing_services_attributes_0_price">Price</label>
