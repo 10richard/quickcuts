f6adab40e724efacd35145702c0906eca7f94b83
Richard Ten <rten04@icloud.com>
2023-08-29 12:56:15 -0400

    Add "dependent: :destroy" for :pictures

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
 
     belongs_to :barber, class_name: 'User'
     
-    has_many_attached :pictures
+    has_many_attached :pictures, dependent: :destroy
 
     has_many :services, dependent: :destroy
     accepts_nested_attributes_for :services, allow_destroy: true,
